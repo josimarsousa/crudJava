7e05f6367094832a13832853db912aba871f71df
Josimar <josimarjosesousa@gmail.com>
2020-10-21 12:49:26 -0300

    alteracao tamanho campo textfield

--- Cadastro/src/forms/formCliente.java
+++ Cadastro/src/forms/formCliente.java
@@ -45,16 +45,16 @@ public class formCliente extends JFrame{
 		jlcod.setBounds(20,20,100,15);
 		jtcod.setBounds(20, 40, 80, 25);
 		jlnome.setBounds(115,20,100,15);
-		jtnome.setBounds(115,40,80,25);
+		jtnome.setBounds(115,40,300,25);
 		jlendereco.setBounds(20,70,100,15);
-		jtendereco.setBounds(20,90,150,25);
+		jtendereco.setBounds(20,90,300,25);
 		jlfone.setBounds(20,120,300,15);
-		jtfone.setBounds(20,140,250,25);
+		jtfone.setBounds(20,140,120,25);
 		jlcpf.setBounds(280,120,150,15);
 		jtcpf.setBounds(280,140,200,25);
-		
-		
+			
 	}
+	
 	public static void main (String args[]) {
 			new formCliente().setVisible(true);
 		}
