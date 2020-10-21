33da9a5c58ee2ef635706ce17d89aa3108088d17
Josimar <josimarjosesousa@gmail.com>
2020-10-21 12:46:06 -0300

    Cadastro cliente ronto

--- Cadastro/src/forms/formCliente.java
+++ Cadastro/src/forms/formCliente.java
@@ -28,5 +28,34 @@ public class formCliente extends JFrame{
 		jlcpf = new JLabel("Cpf:");
 		jtcpf= new JTextField("");
 		
+		getContentPane().setLayout(null);
+		
+		
+		getContentPane().add(jlcod);
+		getContentPane().add(jtcod);
+		getContentPane().add(jlnome);
+		getContentPane().add(jtnome);
+		getContentPane().add(jlendereco);
+		getContentPane().add(jtendereco);
+		getContentPane().add(jlfone);
+		getContentPane().add(jtfone);
+		getContentPane().add(jlcpf);
+		getContentPane().add(jtcpf);
+		
+		jlcod.setBounds(20,20,100,15);
+		jtcod.setBounds(20, 40, 80, 25);
+		jlnome.setBounds(115,20,100,15);
+		jtnome.setBounds(115,40,80,25);
+		jlendereco.setBounds(20,70,100,15);
+		jtendereco.setBounds(20,90,150,25);
+		jlfone.setBounds(20,120,300,15);
+		jtfone.setBounds(20,140,250,25);
+		jlcpf.setBounds(280,120,150,15);
+		jtcpf.setBounds(280,140,200,25);
+		
+		
 	}
+	public static void main (String args[]) {
+			new formCliente().setVisible(true);
+		}
 }
