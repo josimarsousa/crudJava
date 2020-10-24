bed4aea56afe6dfc9a3c124412e5d54f1c102cd7
Josimar <josimarjosesousa@gmail.com>
2020-10-24 16:26:52 -0300

    cadastro fornecedores adicionando

--- Cadastro/src/forms/formFornecedores.java
+++ Cadastro/src/forms/formFornecedores.java
@@ -1,5 +1,30 @@
 package forms;
 
-public class formFornecedores {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class formFornecedores extends JFrame{
+	
+	JLabel jlcodFor, jlnomeFor, jlenderecoFor, jlcnpjFor, jlfoneFor;
+	JTextField jtcodFor, jtnomeFor, jtenderecoFor, jtcnpjFor, jtfoneFor;
+	JButton jblimpar, jbsalvar, jbsair;
+	
+	public formFornecedores() {
+		setTitle("Cadastro de Fornecedores");
+		setSize(550,450);	
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
+		
+		getContentPane().add(jlcodFor);
+		getContentPane().add(jtcodFor);
+		getContentPane().add(jlnomeFor);
+		getContentPane().add(jtnomeFor);
+		getContentPane().add(jlenderecoFor);
+		get
+		
+		
+		
 
 }
