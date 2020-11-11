8e30d6bad48dd3caea9238c5b9d4e3015cd946cc
Josimar <josimarjosesousa@gmail.com>
2020-11-10 22:56:59 -0300

    update classe conexao

--- Cadastro/src/conexao/Conexao.java
+++ Cadastro/src/conexao/Conexao.java
@@ -1,133 +1,41 @@
 package conexao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
-public class Conexao {
+import java.sql.*;
+
+
+public class Conexao{
 	
-	public static String status = "Nao conectado..";
-	
-	public Conexao() {
+	public static void main(String[] args) {
 		
-	}
-	
-	public static java.sql.ConnectionBuilder getConexao(){
-		Connection connection = null;
 		
-		try {
-
-			// Carregando o JDBC Driver padrão
-
-			String driverName = "com.mysql.jdbc.Driver";
-
-			Class.forName(driverName);
-
-
-
-			// Configurando a nossa conexão com um banco de dados//
-
-			        String serverName = "localhost";    //caminho do servidor do BD
-
-			        String mydatabase =”mysql”;        //nome do seu banco de dados
-
-			        String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
-
-			        String username = "root";        //nome de um usuário de seu BD
-
-			        String password = "123456";      //sua senha de acesso
-
-			        connection = DriverManager.getConnection(url, username, password);
-
-
-
-			        //Testa sua conexão//
-
-			        if (connection != null) {
-
-			            status = ("STATUS--->Conectado com sucesso!");
-
-			        } else {
-
-			            status = ("STATUS--->Não foi possivel realizar conexão");
-
-			        }
-
-
-
-			        return connection;
-
-
-
-			        } catch (ClassNotFoundException e) {  //Driver não encontrado
-
-
-
-			            System.out.println("O driver expecificado nao foi encontrado.");
-
-			            return null;
-
-			        } catch (SQLException e) {
-
-			//Não conseguindo se conectar ao banco
-
-			            System.out.println("Nao foi possivel conectar ao Banco de Dados.");
-
-			            return null;
-
-			        }
-
-
-
-			    }
-
-
-
-			    //Método que retorna o status da sua conexão//
-
-			    public static String statusConection() {
-
-			        return status;
-
-			    }
-
-
-
-			   //Método que fecha sua conexão//
-
-			    public static boolean FecharConexao() {
-
-			        try {
-
-			            Conexao.getConexao().close();
-
-			            return true;
-
-			        } catch (SQLException e) {
-
-			            return false;
-
-			        }
-
-
-
-			    }
-
-
-
-			   //Método que reinicia sua conexão//
-
-			    public static java.sql.Connection ReiniciarConexao() {
-
-			        FecharConexao();
-
-
-
-			        return Conexao.getConexao();
-
-			    }
-
-			}
-	}
-
-}
+		        Connection conn = null;
+
+		        try
+		        {
+		            String userName = "root";
+		            String password = "";
+		            String url = "jdbc:mysql://localhost/Control";
+		            Class.forName ("com.mysql.jdbc.Driver").newInstance ();
+		            conn = DriverManager.getConnection (url, userName, password);
+		            System.out.println ("Conexão com o BD estabelecida!");
+		        }
+		        catch (Exception e)
+		        {
+		            System.err.println ("Não foi possível estabelecer conexão com o BD");
+		        }
+		        finally
+		        {
+		            if (conn != null)
+		            {
+		                try
+		                {
+		                    conn.close ();
+		                    System.out.println ("Conexão finalizada");
+		                }
+		                catch (Exception e) { /* ignore close errors */ }
+		            }
+		        }
+		        
+		    }
+		}
